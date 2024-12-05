d30406f5a57415f67f9b348b25fcd7a5ad7b3537
vcx4ka <vcx4ka@virginia.edu>
2024-12-05 12:39:30 -0500

    finished step 4 genres and songs

--- app/main.py
+++ app/main.py
@@ -3,14 +3,72 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import mysql.connector
+from mysql.connector import Error
 import json
 import os
 
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "vcx4ka"
+
+
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")  # zone apex
 def zone_apex():
     return {"Good Day": "Sunshine!"}
+
+@app.get('/genres')
+def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
+
+
+@app.get('/songs')
+def get_songs():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre=genres.genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
+
 
 @app.get("/sum/{a}/{b}")
 def add(a: int, b: int):
